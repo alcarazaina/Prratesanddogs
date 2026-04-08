7c7e78920b397c8d207e2779dd820df92370a822
Ainara Alcaraz <alcarazdainara@gmail.com>
2026-04-08 19:06:35 +0200

    script basico, footer, etc

---
+++ js/main.js
@@ -0,0 +1,12 @@
+async function loadComponent(id, file) {
+  try {
+    const res = await fetch(file);
+    const html = await res.text();
+    document.getElementById(id).innerHTML = html;
+  } catch (error) {
+    console.error("Error cargando componente:", file);
+  }
+}
+
+loadComponent("navbar", "/components/navbar.html");
+loadComponent("footer", "/components/footer.html");
